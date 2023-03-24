db9e3eb9159cd47238462770232e8c61023ca77d
Gabriel Dias <gabriel_dias_santiago@hotmail.com>
2023-03-23 22:27:22 -0300

    first commit

---
+++ .history/main_20230323214328.py
@@ -0,0 +1,31 @@
+import discord
+import openai
+
+
+DISCORD_TOKEN = "SEU_TOKEN_DO_DISCORD_AQUI"
+OPENAI_API_KEY = "SUA_CHAVE_API_DO_OPENAI_AQUI"
+openai.api_key = OPENAI_API_KEY
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message.content,
+        temperature=0.5,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        timeout=10,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    await message.channel.send(response.choices[0].text)
+client.run(DISCORD_TOKEN)
---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+import openai
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+token = ''
+openai.api_key = ''
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("Eu estou logado em {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+
+    print(username + " said " + user_message.lower() + " in " + channel)
+
+    if message.channel.name == client.user:
+        return
+ #em copilot - você deve inserir o canal deve enviar a resposta
+    if message.channel.name == 'copilot': 
+        response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=user_message,
+                max_tokens=3000,
+                temperature=0.7
+            )
+
+        output = response["choices"][0]["text"]
+
+        print(output)
+        await message.channel.send(output)
+
+client.run(token)
